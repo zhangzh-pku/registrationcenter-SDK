959ef26c12a3e22a2d63c281a437160bdb9aceb6
zjgemi <liuxin_zijian@163.com>
2022-11-09 15:37:46 +0800

    support for single, list or dict of locations
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- src/registry/artifacts.py
+++ src/registry/artifacts.py
@@ -1,5 +1,5 @@
 from dflow import S3Artifact
-import json
+
 
 class Artifact:
     @staticmethod
@@ -10,18 +10,6 @@ class Artifact:
             return GitArtifact(**d["git"])
         elif "s3" in d:
             return S3Artifact.from_dict(d["s3"])
-        elif "s3_dict" in d:
-            _d = json.loads(d["s3_dict"])
-            res = {}
-            for k in _d:
-                res[k] = Artifact.from_dict(_d[k])
-            return res
-        elif "s3_list" in d:
-            lis = json.loads(d["s3_list"])
-            res = []
-            for i in lis:
-                res.append(Artifact.from_dict(i))
-            return res
 
 
 class HTTPArtifact(Artifact):
